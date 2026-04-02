a76d9ebbf60eb8ccc79891ddc403573be92eac46
schuh4 <schuh4@jwlogin10.juwels>
2026-04-02 09:46:05 +0200

    test

--- Source/ATFModel/ATFModel.cpp
+++ Source/ATFModel/ATFModel.cpp
@@ -36,21 +36,21 @@ void ATFModel::computeThickeningFactors(
     const amrex::Array4<const amrex::Real> flame_sensors,
     amrex::Array4<amrex::Real> thickening_factors){
 
-	bool use_sensor = do_flame_sensor;
+	bool use_sensor = false; //do_flame_sensor;
 	amrex::Real max_thick = max_thickening;
-	
+
 	AMREX_ASSERT(flame_sensors.contains(bx));
 	AMREX_ASSERT(thickening_factors.contains(bx));
         
-        amrex::ParallelFor(bx, [flame_sensors, thickening_factors, use_sensor, max_thick] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {  
-            if(use_sensor){      
-                amrex::Real omega = flame_sensors(i, j, k);
-                amrex::Real thickening_factor = 1 + (max_thick - 1) * omega;
-                thickening_factors(i,j,k) = thickening_factor;
-            }else{
-                thickening_factors(i,j,k) = max_thick;
-            }
-        });
+    amrex::ParallelFor(bx, [flame_sensors, thickening_factors, use_sensor, max_thick] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {  
+        if(use_sensor){      
+            amrex::Real omega = flame_sensors(i, j, k);
+            amrex::Real thickening_factor = 1 + (max_thick - 1) * omega;
+            thickening_factors(i,j,k) = thickening_factor;
+        }else{
+            thickening_factors(i,j,k) = max_thick;
+        }
+    });
       
 }
 
@@ -59,15 +59,15 @@ void ATFModel::computeEfficiencyFunctions(
     amrex::Array4<amrex::Real> thickening_factors,
     amrex::Array4<amrex::Real> efficiency_functions){
 
-	bool use_efficiency_function = do_flame_sensor;
+	bool use_efficiency_function = false; //do_efficiency_function;
 
-        amrex::ParallelFor(bx, [thickening_factors, efficiency_functions, use_efficiency_function] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {        
-            if(use_efficiency_function){
-                amrex::Real efficiency_function = std::pow(thickening_factors(i,j,k), 2.0/3.0);
-                efficiency_functions(i,j,k) = efficiency_function;
-            }else{
-                efficiency_functions(i,j,k) = 1;
-            }
-        });
+    amrex::ParallelFor(bx, [thickening_factors, efficiency_functions, use_efficiency_function] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {        
+        if(use_efficiency_function){
+            amrex::Real efficiency_function = std::pow(thickening_factors(i,j,k), 2.0/3.0);
+            efficiency_functions(i,j,k) = efficiency_function;
+        }else{
+            efficiency_functions(i,j,k) = 1;
+        }
+    });
 }
 
